7984cc930b3c74eb9ed31e41d5470e6e2ace645a
Lukas Malina <lukas.malina@cern.ch>
2019-10-16 16:43:45 +0200

    Addapted the gui wrapper

--- gui/wrapper_optics.py
+++ gui/wrapper_optics.py
@@ -2,7 +2,8 @@ import sys
 import context
 from hole_in_one import hole_in_one_entrypoint
 
-old_options = ["--tbtana", "--lhcphase", "--errordefs", "--nprocesses", "--average_tune", "--numbpm", "--useerrorofmean", "--errthreshold"]
+old_options = ["--tbtana", "--lhcphase", "--errordefs", "--nprocesses", "--average_tune",
+               "--numbpm", "--useerrorofmean", "--errthreshold", "--bbthreshold", "--cocut"]
 a = sys.argv[1:]
 new_a = []
 for i, b in enumerate(a):
@@ -14,8 +15,7 @@ for i, b in enumerate(a):
             "--accel=PSBOOSTER", "--accel psbooster").replace("--accel=PS", "--accel ps").replace(
             "--bpmu=", "").replace("=", " ").replace(",", " ").replace("twiss.dat", "").replace(
             "--coupling", "--only_coupling").replace("--threebpm", "--three_bpm_method").replace(
-            "--bbthreshold", "--max_beta_beating").replace("--nbcpl", "--coupling_method").replace(
-            "--cocut", "--max_closed_orbit").split())
+            "--nbcpl", "--coupling_method").split())
 new_a.extend(["--optics"])
 hole_in_one_entrypoint(new_a)
 
