9a8cae0247a0e3fe2ffb76399b0525b9a53e5c6f
mihofer <michael.hofer@cern.ch>
2020-01-30 15:08:25 +0100

    added tests for measure optics going through input parameter list
    test partly refactored
    
    added test to check if files are present
    
    clean up added to test, madx script modified to use less space
    
    using madx wrapper to cerate tracking data
    
    removing prev. tracked data

---
+++ tests/accuracy/test_hole_in_one.py
@@ -0,0 +1,116 @@
+from os.path import abspath, dirname, join, isdir
+from shutil import rmtree
+import glob
+from functools import partial
+from omc3.hole_in_one import hole_in_one_entrypoint
+from omc3.madx_wrapper import main as madx_run
+
+PLANES = ('x', 'y')
+
+RESULTS_PATH = abspath(join(dirname(__file__), "..", "results"))
+MODEL_PATH = abspath(join(dirname(__file__), "..", "inputs", "models", "inj_2018_b1"))
+TRACK_FILES = ['track1.one', 'track2.one', 'track3.one']
+OPTICS_MEASUREMENTS_RESULTS_FILES = [
+                 'beta_amplitude_{plane}.tfs',      # List containing all expected results files,
+                 'beta_phase_{plane}.tfs',          # at later point, possibly replaced by importing
+                 'interaction_point_{plane}.tfs',   # from global constants
+                 'kick_{plane}.tfs',
+                 'orbit_{plane}.tfs',
+                 'phase_{plane}.tfs',
+                 'special_phase_{plane}.txt',
+                 'total_phase_{plane}.tfs',
+                ]
+MEASUREMENT_FILES_EXTENSIONS = ['.tfs', '.txt',
+                                '.ampsx', '.ampsy',
+                                '.freqsx', '.freqsy',
+                                '.linx', '.liny',
+                                '.bad_bpms_x', '.bad_bpms_y',
+                                ]
+
+HARPY_RESULTS_FILES = [
+                       '{track}.amps{{plane}}',
+                       '{track}.freqs{{plane}}',
+                       '{track}.lin{{plane}}',
+                       '{track}.bad_bpms_{{plane}}',
+                      ]
+
+RDT_RESULT_FILES = {
+                    'normal_octupole': ['f0013_y.tfs', 'f0040_y.tfs', 'f0211_y.tfs', 'f0220_y.tfs',
+                                        'f1102_x.tfs', 'f1120_x.tfs', 'f1300_x.tfs', 'f2002_x.tfs',
+                                        'f2011_y.tfs', 'f2020_x.tfs', 'f2020_y.tfs', 'f4000_x.tfs'],
+                    'normal_sextupole': ['f0111_y.tfs', 'f0120_y.tfs', 'f1002_x.tfs', 'f1011_y.tfs',
+                                         'f1020_x.tfs', 'f1020_y.tfs', 'f1200_x.tfs', 'f3000_x.tfs'],
+                    'skew_quadrupole': ['f0110_y.tfs', 'f1001_x.tfs', 'f1010_x.tfs', 'f1010_y.tfs'],
+                    'skew_sextupole': ['f0012_y.tfs', 'f0030_y.tfs', 'f0210_y.tfs', 'f1101_x.tfs',
+                                       'f1110_x.tfs', 'f2001_x.tfs', 'f2010_x.tfs', 'f2010_y.tfs'],
+                    }
+
+
+def test_hole_in_one():
+
+    madx_run(dict(file=join(MODEL_PATH, 'model_and_track.madx'), cwd=MODEL_PATH))
+
+    harpy_opt = dict(
+        harpy=True,
+        files=[join(MODEL_PATH, filename) for filename in TRACK_FILES],
+        outputdir=RESULTS_PATH,
+        model=join(MODEL_PATH, 'twiss.dat'),
+        unit='m',
+        turns=[0, 256],
+        to_write=['lin', 'spectra', 'full_spectra', 'bpm_summary'],
+        tbt_datatype='trackone',
+    )
+    clean_opt = dict(
+        clean=False,
+    )
+    fft_opt = dict(
+        autotunes="all",
+        window="hann",
+        turn_bits=12,
+        output_bits=10,
+    )
+    optics_opt = dict(
+        optics=True,
+        coupling_method=0,
+        range_of_bpms=11,
+        nonlinear=True,
+        three_bpm_method=False,
+        compensation="none",
+    )
+    model_opt = dict(
+        accel="lhc",
+        year="2018",
+        ats=False,
+        beam=1,
+        model_dir=MODEL_PATH,
+    )
+
+    hole_in_one_entrypoint({**harpy_opt, **clean_opt, **fft_opt, **optics_opt, **model_opt})
+
+    check_if_all_files_present(RESULTS_PATH,
+                               OPTICS_MEASUREMENTS_RESULTS_FILES)
+
+    check_if_all_files_present(join(RESULTS_PATH, 'lin_files'),
+                               [harpyfiles.format(track=track) for track in TRACK_FILES
+                                                               for harpyfiles in HARPY_RESULTS_FILES])
+
+    [check_if_all_files_present(join(RESULTS_PATH, 'rdt', multipole), RDT_RESULT_FILES[multipole])
+     for multipole in RDT_RESULT_FILES.keys()]
+
+    _clean_up(RESULTS_PATH)
+
+
+def get_all_files_in_dir(path='', fileext=["."]):
+    return [f for ext in fileext for f in glob.glob(join(path, f'*{ext}'))]
+
+
+def check_if_all_files_present(resultpath, resultfiles):
+    generated_results_files = get_all_files_in_dir(resultpath, MEASUREMENT_FILES_EXTENSIONS)
+    test_results_files = [join(resultpath, filename.format(plane=plane)) for plane in PLANES for filename in resultfiles]
+
+    assert set(generated_results_files) == set(test_results_files)
+
+
+def _clean_up(path_dir):
+    if isdir(path_dir):
+        rmtree(path_dir, ignore_errors=True)
